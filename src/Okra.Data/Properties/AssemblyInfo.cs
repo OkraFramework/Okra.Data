591c1e4ea2b2af0d1bbc3b5c21fb164b1e1d4b41
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2014-05-27 20:50:17 +0100

    Update AssemblyInfo

--- src/Okra.Data/Properties/AssemblyInfo.cs
+++ src/Okra.Data/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Okra.Data")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Okra App Framework (Data Framework)")]
+[assembly: AssemblyDescription("The Okra App Framework is designed to support the development of .Net Windows 8 applications, in particular those following the MVVM pattern. This package contains the data assembly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andrew Wilkinson")]
 [assembly: AssemblyProduct("Okra.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Andrew Wilkinson 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.6.0")]
+[assembly: AssemblyFileVersion("0.9.6.0")]
+[assembly: AssemblyInformationalVersion("0.9.6")]
