339323397e52baaf548ec910f622f3e58531b5ce
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2014-07-06 21:04:11 +0100

    Add build script for pushing packages to NuGet.org

---
+++ scripts/Push.ps1
@@ -0,0 +1,27 @@
+[CmdletBinding()]
+Param([Parameter(Mandatory=$True)][Version]$versionNumber)
+
+# Move to the project root folder (parent from current script folder)
+
+function Get-ScriptDirectory
+{ 
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value 
+    Split-Path $Invocation.MyCommand.Path 
+}
+
+$rootFolder = (Get-Item (Get-ScriptDirectory)).Parent.FullName
+Set-Location $rootFolder
+
+# Check NuGet is installed and updated
+
+If (!(Test-Path .\.nuget\nuget.exe))
+{
+    New-Item .\.nuget -type directory -Force | Out-Null
+    Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '.\.nuget\nuget.exe'
+}
+
+.\.nuget\NuGet.exe update -self
+
+# Push packages to NuGet.org
+
+.\.nuget\NuGet.exe push .\artifacts\Okra.Data.$versionNumber.nupkg
